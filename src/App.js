46ae678346fd95c44862b75133805ae8eea6ce86
yuichi <y.y.g.liberty@gmail.com>
2020-05-09 21:21:05 +0900

    App.jsにstyled-componentsを適用

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import "./App.css";
 import Person from "./Person/Person";
+
+const StyledButton = styled.button`
+  background-color: green;
+  color: white;
+  font: inherit;
+  border: 10px solid black;
+  padding: 8px;
+  cursor: pointer;
+  &:hover {
+    background-color: lightgreen;
+    color: black;
+  }
+`;
 
 class App extends Component {
   state = {
@@ -94,9 +108,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm React App!!!</h1>
         <p className={classes.join(" ")}>This is really working!!</p>
-        <button style={style} onClick={() => this.togglePersonsHandler()}>
+        <StyledButton onClick={() => this.togglePersonsHandler()}>
           Toggle Persons
-        </button>
+        </StyledButton>
         {persons}
       </div>
     );
