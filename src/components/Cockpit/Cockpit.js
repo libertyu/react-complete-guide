94306fdd5abe57f5416a8b63f34c727ef360f36a
yuichi <y.y.g.liberty@gmail.com>
2020-06-16 22:14:26 +0900

    useEffect追加

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from "./Cockpit.css";
 
 const cockpit = props => {
+  useEffect(() => {
+    console.log(["[Cockpit.js] useEffect"]);
+    // HTTP Request(例えばAPIを叩くなど)
+  });
+
   let assignClasses = [];
   let btnClass = "";
 
